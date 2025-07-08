b2fbb222f60df090dde8812f2e69c15166eb0ba5
razibdash <avrorazib@gmail.com>
2025-07-08 20:57:39 +0600

    import streamlit,langchain_groq,langchain_core.prompts

---
+++ 01-streamlit-redaction-improver/main.py
@@ -0,0 +1,17 @@
+#streamlit 
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+#model configuration
+model = ChatGroq(
+    api_key=GROQ_API_KEY,
+    model="llama3-70b-8192",
+)
+
+#prompt template
